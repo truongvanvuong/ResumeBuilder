1fcfd0941f2ebbd9859f4ad982aa5e3fe558afa1
vanvuong <truongvuong712219@gmail.com>
2026-06-09 15:12:58 +0700

    update code

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import User from "../models/user.js";
 const protect = async (req, res, next) => {
   try {
     let token = req.headers.authorization;
